f964f6b1cdb53e3270370a140de4b675a087e5c8
Denys Anjos <denysanjospj@hotmail.com>
2022-10-14 19:31:22 -0300

    0.89

--- Exercise089.py
+++ Exercise089.py
@@ -2,35 +2,23 @@
 # e guarde tudo em uma lista composta. no final, mostre um
 # boletim contendo a média de cada um e permita que o usuário
 # possa mostrar as notas de cada aluno individualmente.
-from time import sleep
-
-names = list()
-grades = list()
 all = list()
-
-totalStudents = count = 0
+cont = 0
 while True:
-    n = str(input('Name:'))
-    names.append(n)
-    all.append(names)
-    note1 = float(input('Grade 1:'))
-    grades.append(note1)
-    note2 = float(input('Grade 2:'))
-    grades.append(note2)
-    all.append(grades[:])
-    totalStudents += 1
-
-    check = str(input('Do you want continue?(y/n) '))
+    name = str(input('Type your name:'))
+    grade1 = float(input('Type a grade 1:'))
+    grade2 = float(input('Type a grade 2:'))
+    average = (grade1 + grade2) / 2
+    all.append([name, [grade1, grade2], average])
+    cont += 1
+    check = input('Do you want continue?(y/n)')
     if check in 'nN':
         break
-print(f'N°   NAME    AVERAGE')
-for a in range(0, totalStudents):
-    print(f'{a}    {names[a]}   {(grades[a+a]+grades[a+a+1])/2}')
-    sleep(0.5)
-
+for i, c in enumerate(all):
+    print(f'N°{i:<3} Name:{c[0]:10}Average:{c[2]:<8}')
 while True:
-    check = int(input('Do you wanna show grades of which student? (999 to stop):'))
-    if check != 999:
-        print(f'The grades of {names[check+check]} is {all[check+check]}')
-    else:
+    check = int(input('Do you wanna see the grades of which student?(999 to stop):'))
+    if check == 999:
         break
+    if check <= len(all):
+        print(f'The grades of {all[check][0]} are:{all[check][1]}')
